0f188fe90b1148934c185be06aa1a4aa923d55ac
Khash <khashayar.m.003@gmail.com>
2022-12-02 18:46:25 +0330

    editing

---
+++ react-gallery/src/Config.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const apiKey = `https://www.flickr.com/services/apps/create/noncommercial/?q=${query}&api_key=01384bdb6affad4b55895a0da2c8a573&16&rating=g`;
+
+
+export default apiKey;
