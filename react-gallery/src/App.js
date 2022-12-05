ee6b160b8c640ce7f6c81d5a0c0d54d7cb0a2b4c
Khash <khashayar.m.003@gmail.com>
2022-12-05 16:15:53 +0330

    done

--- react-gallery/src/App.js
+++ react-gallery/src/App.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {Routes, Navigate, Route} from 'react-router-dom';
+import React, { useState} from 'react';
+import {Routes, Route} from 'react-router-dom';
 import axios from 'axios';
 import apiKey from "./config";
 
@@ -8,76 +8,50 @@ import apiKey from "./config";
 import Nav from './Components/Nav';
 import PhotoContainer from './Components/PhotoContainer';
 import SearchForm from './Components/SearchForm';
-import NotFound from './Components/NotFound';
+import RouteNotFound from "./Components/RouteNotFound";
+
 
 
 const App = (props) => {
 
-    const [images, setImages] = useState([]);
-    const [batman, setBatman] = useState([]);
-    const [catwoman, setCatwoman] = useState([]);
-    const [venom, setVenom] = useState([]);
-    const [loading, setloading] = useState(true)
-    
+    const [photos, setPhotos] = useState([]);
+    const [loading, setLoading] = useState(true);
+  
+    const performSearch = (query) => {
+      setLoading(true);
 
-
-
-
-    const performSearch = (keyword = "batman") => {
-        setloading(true);
-        
-        axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${keyword}&per_page=24&format=json&nojsoncallback=1`)
-        .then((response) => {
-            if (keyword === 'batman'){
-                setBatman(response.data.photos.photo);
-                setloading(false)
-            } else if (keyword === 'catwoman') {
-                setCatwoman(response.data.photos.photo);
-                setloading(false)
-            } else if (keyword === 'venom') {
-                setVenom(response.data.photos.photo)
-                setloading(false)
-            } else {
-                setImages(response.data.photos.photo)
-                setloading(false)
-            }
+      axios({
+        url: `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`,
+        method: "get",
+        timeout: 8000
+      })
+        .then(res => {
+          setPhotos(
+            res.data.photos.photo
+          )
+          setLoading(false);
         })
-        .catch((error) => {
-            console.log("Error fetching and parsing dara", error);
-        });
-    };
-
-
-    useEffect(() => {
-        performSearch();
-        performSearch('batman');
-        performSearch('catwoman');
-        performSearch('venom');
-    },[]);
+        .catch(err => {
+          console.log(err);
+        })
+    }
 
 
 
-    
 
     return (
-        <div className='container'>
-            <SearchForm search={performSearch} />
-            <Nav />
-
-        
-            {
-                (loading)
-                ?<h2>Loading...</h2>
-                : <Routes>
-                <Route path='/' element={<Navigate replace to="/batman" />} />
-                <Route path='/batman' element={<PhotoContainer data={batman} /> } />
-                <Route path='/catwoman' element={<PhotoContainer data={catwoman} /> } />
-                <Route path='/venom' element={<PhotoContainer data={venom} /> } />
-                <Route path="/search/:keyword" element={<PhotoContainer data={images} />} />
-                <Route path='*' element={<NotFound />} />
-            </Routes>
-            }
-        </div>
+        <div className="container">
+        <SearchForm />
+        <Nav />
+        <Routes>
+          <Route path="/" element={<PhotoContainer getPhotos={performSearch} photos={photos} loading={loading} />} />
+          <Route path="batman" element={<PhotoContainer defaultTopic={"batman"} getPhotos={performSearch} photos={photos} loading={loading} />} />
+          <Route path="catwoman" element={<PhotoContainer defaultTopic={"catwoman"} getPhotos={performSearch} photos={photos} loading={loading} />} />
+          <Route path="venom" element={<PhotoContainer defaultTopic={"venom"} getPhotos={performSearch} photos={photos} loading={loading} />} />
+          <Route path="search/:topic" element={<PhotoContainer getPhotos={performSearch} photos={photos} loading={loading} />} />
+          <Route path="*" element={<RouteNotFound />} />
+        </Routes>
+      </div>
         
     )
 }
--- react-gallery/src/Components/PhotoContainer.js
+++ react-gallery/src/Components/PhotoContainer.js
@@ -1,34 +1,48 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import { useParams } from "react-router-dom";
 import Photo from './Photo';
 import NotFound from './NotFound';
 
 
 
-
-const PhotoContainer = ({data}) => {
-    const photoData = data;
-    let photoList;
-
-    if (photoData.length > 0) {
-         photoList = photoData.map( image => 
-            <Photo 
-                data={image}
-                key={image.id}
-            />);
-    } else {
-        photoList = <NotFound />
-    }
-   
+    const PhotoContainer = ({ defaultTopic,photos, getPhotos ,loading}) => {
+        const { topic } = useParams();
+        let topicToFetch = topic ? topic : defaultTopic;
+        topicToFetch = topicToFetch ? topicToFetch : "batman%2Ccatwoman%2Cvenom";
+    
+        useEffect(() => {
+            getPhotos(topicToFetch);
+        }, [topicToFetch]);
+    
+        photos = photos.map(photo => {
+            return <Photo
+                url={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+                key={photo.id}
+            />
+        })
      
        
     
     
 
     return (
+       
         <div className="photo-container">
-            <h1>Photo Gallery</h1>
-            <ul>{photoList}</ul>
-        </div>
+        {(loading) ? <h2>Loading...</h2> : null}
+        {(photos.length !== 0 && !loading) ? <h2>Results</h2> : null}
+        <ul>
+            {
+                (photos.length !== 0 && !loading)
+                ? photos
+                : (photos.length === 0 && !loading)
+                ? <NotFound topic={topicToFetch} />
+                : null}
+        </ul>
+    </div>
+   
     );
+
 };
+
+
 export default PhotoContainer
